380d1824a84cee7e737b9750227c8f9e8d4d1798
Willian Ferreira <42327168+will1314robo@users.noreply.github.com>
2024-04-22 16:12:34 -0300

    Update RA2920482221005.java
    Inserção e apresentação dos dados relacionados ao aluno e aos dados pedidos pela atividade

--- RA2920482221005.java
+++ RA2920482221005.java
@@ -1,3 +1,16 @@
 public class RA2920482221005{
-    
+    public static void main(String args[]){
+        String nome="Willian Ferreira dos Santos Costa";
+        String RA="2920482221005";
+        int idade=20;
+        String github="https://github.com/will1314robo";
+        String linkedin="https://www.linkedin.com/in/willian-ferreira-0265a7224/";
+        String cursoturma="ADS - Turma 4º Semestre 2024";
+        System.out.println(nome);
+        System.out.println(RA);
+        System.out.println(idade);
+        System.out.println(github);
+        System.out.println(linkedin);
+        System.out.println(cursoturma);
+    }
 }
